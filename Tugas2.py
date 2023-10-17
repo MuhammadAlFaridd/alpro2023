a6312c48a42b7a82ec4cd1066edc31366e4c67a3
MuhammadAlFaridd <alfarid.22315038@umkendari.ac.id>
2023-10-17 09:59:39 +0800

    Tugas Selesai

---
+++ Tugas2.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+	# tupple
+	x = ( 'Apel', 'Belimbing', 'Ceri')
+	# list
+	y = [ 'Apel', 'Belimbing', 'ceri']
+	# dictionary
+	z = {'a': 'Apel' , 'b': 'Belimbing' , 'c': 'ceri'}
+	
+	print(type(x))
+	print(x)
+	print(type(y))
+	print(y)
+	print(type(z))
+	print(z)
