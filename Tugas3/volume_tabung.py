56b9d8a119827cf1a3ed540760eb08e1c6d62c8c
Muhammad Al Farid 22315038 <alfarid.22315038@umkendari.ac.id>
2023-11-07 21:58:56 +0800

    Tugas 3 selesai

---
+++ Tugas3/volume_tabung.py
@@ -0,0 +1,7 @@
+#rumus VT = pi*r*r*t
+if __name__ == "__main__":
+	pi = 22/7
+	r = int(input("masukan jari jari: "))
+	t = int(input("masukan tingginya: "))
+	VT = pi*r*r*t
+	print("jumlah volume tabung adalah =", VT, "cm3")
